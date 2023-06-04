4b06a6f6af1be5548e6dece55a176084d53acbee
DiegoFernandoTrujilloAlvarez <diego.trujilloalvarez@gmail.com>
2023-06-04 16:58:31 -0500

    Reto 6 - adelanto

---
+++ Reto_7_El_sombrero_seleccionador.py
@@ -0,0 +1,24 @@
+"""
+Crea un programa que simule el comportamiento del sombrero selccionador del
+universo mágico de Harry Potter.
+- De ser posible realizará 5 preguntas (como mínimo) a través de la terminal.
+- Cada pregunta tendrá 4 respuestas posibles (también a selecciona una a través de terminal).
+- En función de las respuestas a las 5 preguntas deberás diseñar un algoritmo que
+  coloque al alumno en una de las 4 casas de Hogwarts (Gryffindor, Slytherin , Hufflepuff y Ravenclaw)
+- Ten en cuenta los rasgos de cada casa para hacer las preguntas y crear el algoritmo seleccionador.
+  Por ejemplo, en Slytherin se premia la ambición y la astucia.
+
+Edit:
+Se cambia la prueba a elegir que linea del juego League Of Leguends debería jugar
+  
+"""
+
+PuntosTop = 0
+PuntosJg = 0
+PuntosMid = 0
+PuntosAdc = 0
+PuntosSup = 0
+
+
+
+
